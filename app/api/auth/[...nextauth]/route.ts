c660d7e289b655edd229229da32e777bad5bbd7d
ratulhasan2a <ratulhasan5a@gmail.com>
2026-05-16 17:07:34 +0000

    fix: update next-auth configuration

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import clientPromise from "../../../../../lib/mongodb";
+import CredentialsProvider from "next-authprovidersoviders/credentials";
+import clientPromise from "@/lib/mongodb";
 import bcrypt from "bcryptjs";
 
 const handler = NextAuth({
