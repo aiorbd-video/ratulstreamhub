ec78851e6e719943405cfd6ce41eb0c513487c17
ratulhasan2a <ratulhasan5a@gmail.com>
2026-05-16 17:10:39 +0000

    fix: correct typo in next-auth route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-authprovidersoviders/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
 import clientPromise from "@/lib/mongodb";
 import bcrypt from "bcryptjs";
 
