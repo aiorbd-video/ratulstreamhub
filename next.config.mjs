f8df944a21e64d3dd8a2545de83cc9d42a7c2885
aiorbd-video <ratulhasan5a@gmail.com>
2026-05-18 21:02:26 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/player_api.php',
-        destination: '/api/xtream',
-      },
-      {
-        // 🎯 ম্যাজিক: যদি অ্যাপ /live ছাড়া ডিরেক্ট লিংক পাঠায় (Smarters Pro এর ডিফল্ট রিকোয়েস্ট)
-        source: '/:username([0-9]{10,15})/:password/:stream_id',
-        destination: '/api/play_xtream?username=:username&password=:password&stream_id=:stream_id',
-      },
-      {
-        // 🎯 ম্যাজিক: যদি অ্যাপ /live যুক্ত করে লিংক পাঠায় (TiviMate এর রিকোয়েস্ট)
-        source: '/live/:username/:password/:stream_id',
-        destination: '/api/play_xtream?username=:username&password=:password&stream_id=:stream_id',
-      }
-    ];
-  },
-};
-
+const nextConfig = {};
 export default nextConfig;
